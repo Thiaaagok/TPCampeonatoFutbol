975c393cc0ac13343405ff769ab43dc317244b90
Thiago Valdez <thiago@kaitsoft.com.ar>
2025-05-08 12:01:16 -0300

    Primer commit

---
+++ TPCampeonatoFutbol/LoginForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace TPCampeonatoFutbol
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NombreUsuariolbl = new System.Windows.Forms.Label();
+            this.Contraseñalbl = new System.Windows.Forms.Label();
+            this.NombreUsuariotxt = new System.Windows.Forms.TextBox();
+            this.Contraseñatxt = new System.Windows.Forms.TextBox();
+            this.IniciarSesionbtn = new System.Windows.Forms.Button();
+            this.crearCuentaBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NombreUsuariolbl
+            // 
+            this.NombreUsuariolbl.AutoSize = true;
+            this.NombreUsuariolbl.Location = new System.Drawing.Point(325, 99);
+            this.NombreUsuariolbl.Name = "NombreUsuariolbl";
+            this.NombreUsuariolbl.Size = new System.Drawing.Size(98, 13);
+            this.NombreUsuariolbl.TabIndex = 0;
+            this.NombreUsuariolbl.Text = "Nombre de Usuario";
+            this.NombreUsuariolbl.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Contraseñalbl
+            // 
+            this.Contraseñalbl.AutoSize = true;
+            this.Contraseñalbl.Location = new System.Drawing.Point(325, 143);
+            this.Contraseñalbl.Name = "Contraseñalbl";
+            this.Contraseñalbl.Size = new System.Drawing.Size(61, 13);
+            this.Contraseñalbl.TabIndex = 2;
+            this.Contraseñalbl.Text = "Contraseña";
+            // 
+            // NombreUsuariotxt
+            // 
+            this.NombreUsuariotxt.Location = new System.Drawing.Point(328, 115);
+            this.NombreUsuariotxt.Name = "NombreUsuariotxt";
+            this.NombreUsuariotxt.Size = new System.Drawing.Size(100, 20);
+            this.NombreUsuariotxt.TabIndex = 3;
+            // 
+            // Contraseñatxt
+            // 
+            this.Contraseñatxt.Location = new System.Drawing.Point(328, 159);
+            this.Contraseñatxt.Name = "Contraseñatxt";
+            this.Contraseñatxt.Size = new System.Drawing.Size(100, 20);
+            this.Contraseñatxt.TabIndex = 4;
+            // 
+            // IniciarSesionbtn
+            // 
+            this.IniciarSesionbtn.Location = new System.Drawing.Point(328, 194);
+            this.IniciarSesionbtn.Name = "IniciarSesionbtn";
+            this.IniciarSesionbtn.Size = new System.Drawing.Size(100, 23);
+            this.IniciarSesionbtn.TabIndex = 5;
+            this.IniciarSesionbtn.Text = "Iniciar Sesión";
+            this.IniciarSesionbtn.UseVisualStyleBackColor = true;
+            // 
+            // crearCuentaBtn
+            // 
+            this.crearCuentaBtn.Location = new System.Drawing.Point(208, 194);
+            this.crearCuentaBtn.Name = "crearCuentaBtn";
+            this.crearCuentaBtn.Size = new System.Drawing.Size(114, 23);
+            this.crearCuentaBtn.TabIndex = 6;
+            this.crearCuentaBtn.Text = "Crear cuenta";
+            this.crearCuentaBtn.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.crearCuentaBtn);
+            this.Controls.Add(this.IniciarSesionbtn);
+            this.Controls.Add(this.Contraseñatxt);
+            this.Controls.Add(this.NombreUsuariotxt);
+            this.Controls.Add(this.Contraseñalbl);
+            this.Controls.Add(this.NombreUsuariolbl);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NombreUsuariolbl;
+        private System.Windows.Forms.Label Contraseñalbl;
+        private System.Windows.Forms.TextBox NombreUsuariotxt;
+        private System.Windows.Forms.TextBox Contraseñatxt;
+        private System.Windows.Forms.Button IniciarSesionbtn;
+        private System.Windows.Forms.Button crearCuentaBtn;
+    }
+}
